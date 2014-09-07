56bc129777aa3ecc3b135bcaf42f3a9f421a66fd
Stanislau Charniakou <stas.cherniakov@gmail.com>
2014-09-07 15:58:27 -0700

    change parser behaviour: different starting offset

--- utils/schedule_parser.py
+++ utils/schedule_parser.py
@@ -8,7 +8,15 @@ from dateutil.relativedelta import relativedelta
 
 import leaf
 
-WEEKDAYS = {u'понедельник': 1, u'вторник': 2, u'среда': 3, u'четверг': 4, u'пятница': 5, u'суббота': 6}
+WEEKDAYS = {
+    u'понедельник': 1,
+    u'вторник': 2,
+    u'среда': 3,
+    u'четверг': 4,
+    u'пятница': 5,
+    u'суббота': 6
+}
+
 MAIN_TABLE_PATTERN = re.compile(r'<table\b.*?>.*?</table>', re.DOTALL)
 
 
@@ -17,13 +25,30 @@ def get_semester_start_date():
     year_start = date(current_date.year, 1, 1)
     if current_date.month >= 8 or current_date.month == 1:
         # if it's past august - semester would start
-        # todo: first week fluctuation problem
         semester_start = year_start + relativedelta(month=9, day=1)
     else:
         # usually it's the first monday of february for the second semester, but we need a day before
-        semester_start = year_start + relativedelta(month=2, weekday=SU(0))
+        semester_start = year_start + relativedelta(month=2, weekday=MO(0))
 
     return datetime(semester_start.year, semester_start.month, semester_start.day)
+
+
+def get_date_offset(current_week, current_day):
+    return timedelta(weeks=int(current_week) - 1, days=int(current_day) - 1)
+
+
+def get_document(raw_html_schedule):
+    return leaf.parse(leaf.strip_symbols(leaf.strip_accents(show(raw_html_schedule))))
+
+
+def get_rows(document):
+    return document.get('table')('tr')
+
+
+def parse_header(header_td):
+    current_week = re.findall('\w+(?=\-)', header_td.text)[0]
+    current_day = WEEKDAYS[header_td.text.split(u',')[0]]
+    return current_week, current_day
 
 
 def show(raw_html_schedule):
@@ -39,27 +64,18 @@ def read(raw_html_schedule):
     """
     semester_start_date = get_semester_start_date()
     schedule = []
-    check = False
-    document = leaf.parse(leaf.strip_symbols(leaf.strip_accents(show(raw_html_schedule))))
-    table = document.get('table')
-    trs = table('tr')
-    for tr in trs:
+    document = get_document(raw_html_schedule)
+    for tr in get_rows(document):
         schedule_class = {}
         tds = tr.xpath('td')
-        for td in tds:
-            if td.colspan == '3':
-                current_week = re.findall('\w+(?=\-)', td.text)[0]
-                current_day = WEEKDAYS[td.text.split(u',')[0]]
-                check = False
-            else:
-                check = True
-
-        if check:
-            if not u'подгр.' in tds[0].text:
+        if len(tds) == 1 and tds[0].colspan == '3':
+            # one element and colspan usually means a header
+            current_week, current_day = parse_header(tds[0])
+        else:
+            if not u'подгр.' in tds[0].text and (current_week and current_day):
                 schedule_class['subject'] = tds[1].text.rstrip(u' (')
 
-                delta = timedelta(weeks=int(current_week) - 1, days=int(current_day))
-                start_date = semester_start_date + delta
+                start_date = semester_start_date + get_date_offset(current_week, current_day)
                 time_string = tds[0].text.split(u'-')
                 start_time = timedelta(hours=int(time_string[0].split(u':')[0]),
                                        minutes=int(time_string[0].split(u':')[1]))
