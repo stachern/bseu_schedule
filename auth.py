2641bd6193a157e5fe648ab300958519951401bc
Dzianis Dashkevich <dskecse@gmail.com>
2023-12-22 15:24:58 +0300

    Remove unused import

--- auth.py
+++ auth.py
@@ -13,7 +13,6 @@ from gaesessions import get_current_session
 
 import requests
 import requests_toolbelt.adapters.appengine
-from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import Flow
 from googleapiclient.discovery import build
 
