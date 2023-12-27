5023ffc2d2698e9af6156c1b2c828f8614bc1b8d
Dzianis Dashkevich <dskecse@gmail.com>
2023-12-27 17:23:04 +0300

    Enable legacy bundled services on Python3 runtime
    * https://cloud.google.com/appengine/docs/standard/python3/services/access

--- appengine_config.py
+++ appengine_config.py
@@ -1,14 +1,14 @@
-from google.appengine.ext import vendor
+# from google.appengine.ext import vendor
 
-# Add any libraries installed in the "lib" folder
-vendor.add('lib')
+# # Add any libraries installed in the "lib" folder
+# vendor.add('lib')
 
-from gaesessions import SessionMiddleware
+# from gaesessions import SessionMiddleware
 
-COOKIE_KEY = 'oib23b234,mnasd[f898yhk4jblafiuhd2jk341m2n3vb'
+# COOKIE_KEY = 'oib23b234,mnasd[f898yhk4jblafiuhd2jk341m2n3vb'
 
-def webapp_add_wsgi_middleware(app):
-    from google.appengine.ext.appstats import recording
-    app = SessionMiddleware(app, cookie_key=COOKIE_KEY)
-    app = recording.appstats_wsgi_middleware(app)
-    return app
+# def webapp_add_wsgi_middleware(app):
+#     from google.appengine.ext.appstats import recording
+#     app = SessionMiddleware(app, cookie_key=COOKIE_KEY)
+#     app = recording.appstats_wsgi_middleware(app)
+#     return app
